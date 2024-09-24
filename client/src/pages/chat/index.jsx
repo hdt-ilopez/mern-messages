8616c252276c0812c50fc29f707c69e730b7c9a3
hdt-ilopez <150853590+hdt-ilopez@users.noreply.github.com>
2024-09-24 12:16:12 -0500

    Added componenets for contacts, empty chat state, and filled chat state.

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,5 +1,16 @@
+import ContactsContainer from '@/pages/chat/components/contacts-container';
+import EmptyChatContainer from '@/pages/chat/components/empty-chat-container';
+import ChatContainer from '@/pages/chat/components/chat-container';
+
 const Chat = () => {
-  return <div>Chat</div>;
+  const selectedMessage = false;
+
+  return (
+    <div className="flex h-screen text-white overflow-hidden">
+      <ContactsContainer />
+      {selectedMessage ? <ChatContainer /> : <EmptyChatContainer />}
+    </div>
+  );
 };
 
 export default Chat;
