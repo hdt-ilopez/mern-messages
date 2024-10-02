515a0e278b344584b1cd0df1b4ab7f114542cf48
hdt-ilopez <150853590+hdt-ilopez@users.noreply.github.com>
2024-10-02 08:11:01 -0500

    Adjusted file structure and added build, dev, and start scripts

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-// Import necessary modules
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -6,39 +5,29 @@ import mongoose from 'mongoose';
 import morgan from 'morgan';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { app, server } from './socket.js'; // Ensure socket.js exports both app and server
-
-// Import routes
+import { app, server } from './socket.js';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import chatRoutes from './routes/chat.routes.js';
 
-// Initialize environment variables
 dotenv.config();
 
-// Define PORT
 const PORT = process.env.PORT || 5000;
-
-// Define __dirname for ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Middleware setup
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('common'));
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 
-// Serve static files
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
-// Handle SPA routing, return index.html for any unknown routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 mongoose
@@ -50,5 +39,5 @@ mongoose
   })
   .catch((error) => {
     console.error(`Database connection failed: ${error}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   });
