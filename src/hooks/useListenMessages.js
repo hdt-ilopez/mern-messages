574f248f698ae8bf5fb011e4b52fc7879e4fdedf
hdt-ilopez <150853590+hdt-ilopez@users.noreply.github.com>
2024-10-02 08:30:15 -0500

    Added sounds on message recieved

--- src/hooks/useListenMessages.js
+++ src/hooks/useListenMessages.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSocketContext } from '@/contexts/SocketContext';
 import { useChatStore } from '@/store';
 import { useChat } from './useChat';
+import notificationSound from '../assets/sounds/notification.mp3';
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
@@ -11,6 +12,8 @@ const useListenMessages = () => {
   useEffect(() => {
     socket?.on('newMessage', (newMessage) => {
       newMessage.shouldShake = true;
+      const sound = new Audio(notificationSound);
+      sound.play();
       getConversations();
       setMessages([...messages, newMessage]);
     });
